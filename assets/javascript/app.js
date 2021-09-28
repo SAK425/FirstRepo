67bdd376a648000fa323cd2984d2a0d920ee7a5f
SAK425 <sakshidarshane2017@gmail.com>
2021-09-28 18:57:03 +0530

    javascript datatypes

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//string
+console.log(typeof"Hello World")
+console.log(typeof"d")
+
+//numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 11233456789016273849450056)
+console.log(typeof -1)
+console.log(typeof -1.2)
+console.log(typeof '1')
+
+//boolean
+console.log(true)
+console.log(false)
+console.log(typeof true)
