7f9dfb69a3e89edf899588c7e3ca5ef9788c5db3
SAK425 <sakshidarshane2017@gmail.com>
2021-09-30 20:09:33 +0530

    todolist add method

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,11 +21,36 @@ greet();
 /*var add=(a=0,b=0)=>{
     var sum = a+b
     return sum
-}*/
+}
 var add=(a=0,b=0)=>a+b
 console.log(add(10,20))
 
 const greet =(name)=>{
     return 'hi '+name
 }
-console.log(greet("Sakshi"))
+console.log(greet("Sakshi"))*/
+
+//var arr=['Go to the GYM','Eat Food','Clean House']
+/*for (var i=0;i<arr.length;i++){
+    console.log(arr[i])
+}*/
+/*var callbackFunction = (element,index) =>{
+    console.log(element,index)
+}
+arr.forEach(callbackFunction)*/
+/*arr.forEach((element,index)=>{
+    console.log(element,index)
+})*/
+var button = document.querySelector('button')
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
+const callback = (e)=>{
+    const inputvalue = input.value
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(inputvalue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+    console.log('sdf',e.target)
+    console.log(input.value)
+}
+button.addEventListener('click',callback)
