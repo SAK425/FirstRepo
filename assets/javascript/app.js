2978d0a22c782f456478843e295da83fc7e99040
SAK425 <sakshidarshane2017@gmail.com>
2021-09-30 19:13:22 +0530

    functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,25 @@
-console.log("hello")
+
+function greet(name='John',age=19){
+    console.log(age,name)
+}
+var greet = function(name='John',age=19){
+    console.log(age,name)
+}
+greet();
+//console.log(add(10,20))
+//function add(a = 0, b = 0) {
+  // var sum = a + b
+   //return sum
+//}
+
+/*var add = function (a = 0, b = 0) {
+    var sum = a + b
+    return sum
+}*/
+
+
+var add=(a=0,b=0)=>{
+    var sum = a+b
+    return sum
+}
+console.log(add(10,20))
