6a51336594577fbd781601b0ebf88f62ed4f1654
SAK425 <sakshidarshane2017@gmail.com>
2021-10-01 17:49:10 +0530

    css added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,22 @@
-console.log("hello")
+var inputvalue = document.querySelector('input');
+var button = document.querySelector('#addbtn');
+var list = document.querySelector('ul');
+/*
+const callback = ()=>{
+    const inputvalue = input.value;
+    const element = document.createElement('li')
+    const textnode=document.createTextNode(inputvalue)
+    element.appendChild(textnode)
+    list.appendChild(element)
+}
+button.addEventListener('click',callback)
+*/
+
+
+
+var body=document.querySelector('body')
+var button = document.querySelector('.switch')
+const callback = ()=>{
+body.classList.toggle('dark')
+}
+button.addEventListener('checked',callback)
