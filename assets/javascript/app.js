d1562f0334f2801fcf2bc3e6b159cc867df6954b
SAK425 <sakshidarshane2017@gmail.com>
2021-10-03 12:11:06 +0530

    list func added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,22 +41,38 @@ arr.forEach(callbackFunction)*/
 /*arr.forEach((element,index)=>{
     console.log(element,index)
 })*/
-/*var button = document.querySelector('button')
+var button = document.querySelector('button')
 var input = document.querySelector('input')
 var list = document.querySelector('ul')
+var chores = []
+var deleteItem = (value) =>{
+    const index = chores.indexOf(value)
+   chores.splice(index,1)
+   console.log(chores)
+}
+
+
 const callback = (e)=>{
     const inputvalue = input.value
+    if(chores.includes(inputvalue)){
+        console.log('already exists')
+    }
+    else{
+    chores.push(inputvalue)
     const element = document.createElement('li')
     const textNode = document.createTextNode(inputvalue)
     element.appendChild(textNode)
     list.appendChild(element)
-    console.log('sdf',e.target)
-    console.log(input.value)
+    element.addEventListener('click',(e)=>{
+             e.target.remove()
+    })
 }
-button.addEventListener('click',callback)*/
+}
+button.addEventListener('click',callback)
+/*
 var body=document.querySelector('body')
 var button = document.querySelector('button')
 const callback = (event)=>{
 body.classList.toggle('dark')
 }
-button.addEventListener('click',callback)
+button.addEventListener('click',callback)*/
