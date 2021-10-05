f2e0cbede81c345bfd65d3df03cff5326ece9158
SAK425 <sakshidarshane2017@gmail.com>
2021-10-05 18:07:26 +0530

    add functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,13 @@
 function Appendl(){
-var input = document.querySelector("input")
-var list=document.querySelector("#li_elements")
-
-    const element = document.createElement('li')
-    const inputvalue = input.value
-    const textNode = document.createTextNode(inputvalue)
-    element.appendChild(textNode)
-    list.appendChild(element)
-    document.getElementById.innerHTML = list
+    var input = document.querySelector("#Txtinput").value
+    var list=document.querySelector("#li_elements")
     
-
-}
-
+        const element = document.createElement('li')
+        
+        const textNode = document.createTextNode(input)
+        element.appendChild(textNode)
+        list.appendChild(element)
+        document.getElementById.innerHTML = list
+        
+    
+    }
