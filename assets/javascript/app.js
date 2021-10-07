94ff0787e9f3d9000489dc47d358e4c99a9d8049
SAK425 <sakshidarshane2017@gmail.com>
2021-10-07 15:24:55 +0530

    delete function added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,28 @@
+var mylist = document.getElementsByTagName("li")
+var i;
+for(i=0;i<mylist.length;i++){
+    var span=document.createElement("span")
+    var txt=document.createTextNode("\u00D7");
+    span.className="close";
+    span.appendChild(txt);
+    mylist[i].appendChild(span);
+}
+var close=document.getElementsByClassName("close");
+var i;
+for(i=0;i<close.length;i++){
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+var list = document.querySelector('#li_elments');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'li') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
 function Appendl(){
     var input = document.querySelector("#Txtinput").value
     var list=document.querySelector("#li_elements")
@@ -8,6 +33,17 @@ function Appendl(){
         element.appendChild(textNode)
         list.appendChild(element)
         document.getElementById.innerHTML = list
-        
+        var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  } 
     
     }
