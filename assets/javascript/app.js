5ae8d393caa230cd9d8a249ca4c263d91877c1cb
SAK425 <sakshidarshane2017@gmail.com>
2021-10-09 18:06:19 +0530

    delete function added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,27 @@
+var chores = []
 var mylist = document.getElementsByTagName("li")
-var i;
-for(i=0;i<mylist.length;i++){
-    var span=document.createElement("span")
-    var txt=document.createTextNode("\u00D7");
-    span.className="close";
-    span.appendChild(txt);
-    mylist[i].appendChild(span);
-}
-var close=document.getElementsByClassName("close");
-var i;
-for(i=0;i<close.length;i++){
-    close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-var list = document.querySelector('#li_elments');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
 
 function Appendl(){
     var input = document.querySelector("#Txtinput").value
+    chores.push(input)
     var list=document.querySelector("#li_elements")
     
         const element = document.createElement('li')
-        
+        const countofele = document.getElementById('count')
         const textNode = document.createTextNode(input)
+        const minus = document.createElement('button')
+    
+        countofele.innerHTML = chores.length
+        minus.setAttribute('class','minusbtn')
         element.appendChild(textNode)
+        element.appendChild(minus)
         list.appendChild(element)
         document.getElementById.innerHTML = list
-        var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  } 
+        
+         minus.addEventListener('click',(e)=>{
+             chores = chores.filter(item => item!= input)
+             element.remove()
+             
+         })
     
     }
