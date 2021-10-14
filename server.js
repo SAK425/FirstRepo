b5af3a3c5f227ff2094e341e4301adf768d5e729
SAK425 <sakshidarshane2017@gmail.com>
2021-10-14 13:17:34 +0530

    middleware

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express=require('express')
 const port = 3000
 const app = express()
+app.use()
+app.post('/signup',express.json(),(req,res)=>{
+    console.log(req.body)
+ res.send('dadaaa')
+})
+
+/*
+const isAdmin =(req,res,next)=>{
+    if(req.headers.admin==='true')next()
+    else res.send("unauthorized")
+}
+
+
+
+
+app.get('/public',(req,res)=>{
+    console.log(req.headers)
+    res.send('I am a public route')
+})
+app.get('/private',isAdmin,(req,res)=>{
+    res.send('I am a admin route')
+})*/
+/*
 const verify =(req,res,next)=>{
     if(req.headers['user-agent']=="Thunder Client (https://www.thunderclient.io)")next()
   
@@ -12,7 +35,7 @@ app.get('/',verify,(req,res)=>{
     console.log(req)
   
     
-})
+})*/
 
 
 app.listen(port,()=>{
